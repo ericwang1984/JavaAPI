c3c83896af96da6a2b891805ef69ef98c8c4095a
wanghuanxi@gmail.com <github1095Wang>
2017-04-30 11:46:48 +0800

    reflect sample

---
+++ src/main/java/ReflectSample.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Field;
+
+/**
+ * Created by wanghuanxi on 30/04/2017.
+ */
+public class ReflectSample <T>{
+
+
+
+    public String getValues(T object){
+       Field[] fields =  object.getClass().getDeclaredFields();
+
+        return  null;
+    }
+
+
+
+    /**
+     * 拼接某属性的 get方法
+     * @param fieldName
+     * @return String
+     */
+    private static String parGetName(String fieldName) {
+        if (null == fieldName || "".equals(fieldName)) {
+            return null;
+        }
+        return "get" + fieldName.substring(0, 1).toUpperCase()
+                + fieldName.substring(1);
+    }
+    /**
+     * 拼接在某属性的 set方法
+     * @param fieldName
+     * @return String
+     */
+    private static String parSetName(String fieldName) {
+        if (null == fieldName || "".equals(fieldName)) {
+            return null;
+        }
+        return "set" + fieldName.substring(0, 1).toUpperCase()
+                + fieldName.substring(1);
+    }
+}
